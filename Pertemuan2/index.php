c4df31818f2a4328f987295ed1673ce5893cd18b
Safardoaibu <115675780+Safar250501@users.noreply.github.com>
2023-01-08 20:23:13 +0800

    "KULIAH"

---
+++ Pertemuan2/index.php
@@ -0,0 +1,51 @@
+<?php  
+//standar output di php
+//echo,print,print-f,var_dump
+
+// var_dump(" Hello World!") ;
+
+//variabel
+// $nama = "SAFARUDDIN";
+// echo "Nama saya $nama";
+
+//operator
+//aritmatika
+// * / + - %
+
+// $x = 10;
+// $y = 20;
+// echo $x * $y ;
+// echo $x + $y ;
+// echo $x / $y ;
+// echo $x - $y ;
+
+//penggabungan string/concat
+// $nama_depan ="safar";
+// $nama_belakang ="FAR";
+// echo $nama_depan . " " . $nama_belakang;
+
+//asigment
+// $x = 1;
+// $x %= 5;
+// echo $x;
+
+// $nama = "";
+// $nama .= $nama_depan;
+// $nama .= " ";
+// $nama .= $nama_belakang;
+// echo $nama;
+
+//perbandingan
+// < > <= >= == !=
+var_dump(1 == "1");
+
+//identitas 
+// ===, !==
+var_dump(1 === "1");
+
+//logika
+// &&, ||, !
+$x = 10;
+var_dump($x < 20 && $x % 2 == 0);
+?>
+
